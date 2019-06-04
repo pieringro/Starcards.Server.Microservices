3b24bff4282f40a3fe5986d0fe7407292f7efc2a
pierprogramm <pierprogramm@gmail.com>
2019-06-04 22:58:57 +0200

    rabbitmq setup, start rabbitmq server background

--- Infrastructure/rabbitmq/scripts/rabbitmq-setup.sh
+++ Infrastructure/rabbitmq/scripts/rabbitmq-setup.sh
@@ -2,13 +2,13 @@
 
 echo "!!! Begin RabbitMQ: rabbitmq-setup.sh !!!"
 
+rabbitmq-server &
+
+echo ">>>>> Wait 20 seconds, until rabbitmq is ready."
+sleep 20s
+
 curl -i -u guest:guest -H "content-type:application/json" \
  -XPUT -d'{"type":"fanout","durable":true}' \
  http://localhost:15672/api/exchanges/%2f/my-new-exchange
 
 echo "!!! End RabbitMQ: rabbitmq-setup.sh !!!"
-echo "Enter to continue"
-read
-read
-
-
